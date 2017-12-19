1edf802f25585214b8901065cd65e7131f963af2
Ned Ruggeri <ruggeri@self-loop.com>
2017-12-19 23:49:55 +0000

    give generation network layers names for easier reading

--- src/generation_network.py
+++ src/generation_network.py
@@ -6,26 +6,31 @@ from keras.layers import Add, BatchNormalization, Conv2D, Cropping2D, Input, Lam
 from keras.models import Model
 import tensorflow as tf
 
-def residual_transform_block(input_tensor, num_filters):
+def residual_transform_block(input_tensor, num_filters, block_idx):
     residual = Conv2D(
         filters = num_filters,
         kernel_size = (3, 3),
         strides = (1, 1),
         activation = 'relu',
+        name = f'resblock{block_idx}/conv2d1',
     )(input_tensor)
-    residual = BatchNormalization()(residual)
+    residual = BatchNormalization(name = f'resblock{block_idx}/bn1')(residual)
     residual = Conv2D(
         filters = num_filters,
         kernel_size = (3, 3),
         strides = (1, 1),
         activation = 'relu',
+        name = f'resblock{block_idx}/conv2d2'
     )(residual)
-    residual = BatchNormalization()(residual)
+    residual = BatchNormalization(name = f'resblock{block_idx}/bn2')(residual)
 
     cropped_input = Cropping2D(
-        ((2, 2), (2, 2))
+        ((2, 2), (2, 2)),
+        name = f'resblock{block_idx}/crop'
     )(input_tensor)
-    return Add()([residual, cropped_input])
+    return Add(name = f'resblock{block_idx}/add')(
+        [residual, cropped_input]
+    )
 
 def build(input_shape):
     input_tensor = Input(shape = input_shape)
@@ -40,7 +45,7 @@ def build(input_shape):
             (40, 40),
             (0, 0), # don't pad channels. duh.
         ], 'REFLECT'
-    ))(input_tensor)
+    ), name = 'padding')(input_tensor)
 
     # Begin squishing the image down! First just a stride 1 convolution.
     squished_image = Conv2D(
@@ -49,69 +54,75 @@ def build(input_shape):
         strides = (1, 1),
         padding = 'SAME',
         activation = 'relu',
+        name = 'squish1/conv2d'
     )(padded_input_tensor)
-    squished_image = BatchNormalization()(squished_image)
+    squished_image = BatchNormalization(name = 'squish1/bn')(squished_image)
     squished_image = Conv2D(
         filters = 64,
         kernel_size = (3, 3),
         strides = (2, 2),
         padding = 'SAME',
         activation = 'relu',
+        name = 'squish2/conv2d',
     )(squished_image)
-    squished_image = BatchNormalization()(squished_image)
+    squished_image = BatchNormalization(name = 'squish2/bn')(squished_image)
     squished_image = Conv2D(
         filters = 128,
         kernel_size = (3, 3),
         strides = (2, 2),
         padding = 'SAME',
         activation = 'relu',
+        name = 'squish3/conv2d'
     )(squished_image)
-    squished_image = BatchNormalization()(squished_image)
+    squished_image = BatchNormalization(name = 'squish3/bn')(squished_image)
 
     # Squishing complete! Begin the glorious transformation!
     transformed_image = residual_transform_block(
-        squished_image, num_filters = 128
+        squished_image, num_filters = 128, block_idx = 1,
     )
     transformed_image = residual_transform_block(
-        transformed_image, num_filters = 128
+        transformed_image, num_filters = 128, block_idx = 2,
     )
     transformed_image = residual_transform_block(
-        transformed_image, num_filters = 128
+        transformed_image, num_filters = 128, block_idx = 3,
     )
     transformed_image = residual_transform_block(
-        transformed_image, num_filters = 128
+        transformed_image, num_filters = 128, block_idx = 4,
     )
     transformed_image = residual_transform_block(
-        transformed_image, num_filters = 128
+        transformed_image, num_filters = 128, block_idx = 5,
     )
 
     # Begin the upscaling
     # Bilinear resize recommended by https://distill.pub/2016/deconv-checkerboard/
     upscaled = Lambda(lambda transformed_image: tf.image.resize_bilinear(
         transformed_image,
-        (input_shape[0] // 2, input_shape[1] // 2)
-    ))(transformed_image)
+        (input_shape[0] // 2, input_shape[1] // 2),
+    ), name = 'upscale1/resize')(transformed_image)
     upscaled = Conv2D(
         filters = 64,
         kernel_size = (3, 3),
         strides = (1, 1),
         activation = 'relu',
         padding = 'SAME',
+        name = 'upscale1/conv2d',
     )(upscaled)
-    upscaled = BatchNormalization()(upscaled)
+    upscaled = BatchNormalization(name = 'upscale1/bn')(upscaled)
     # Round 2 of upscaling!
     upscaled = Lambda(lambda upscaled: tf.image.resize_bilinear(
         upscaled,
         (input_shape[0], input_shape[1]),
-    ))(upscaled)
+    ), name = 'upscale2/resize')(upscaled)
     upscaled = Conv2D(
         filters = 64,
         kernel_size = (3, 3),
         strides = (1, 1),
         activation = 'relu',
         padding = 'SAME',
+        name = 'upscale2/conv2d',
     )(upscaled)
-    upscaled = BatchNormalization()(upscaled)
+    upscaled = BatchNormalization(name = 'upscale2/bn')(upscaled)
+
     # One big convolution to finish things up!
     upscaled = Conv2D(
         filters = 3,
@@ -119,6 +130,7 @@ def build(input_shape):
         strides = (1, 1),
         activation = 'relu',
         padding = 'SAME',
+        name = 'final_convolution',
     )(upscaled)
 
     return Model(input_tensor, upscaled)
